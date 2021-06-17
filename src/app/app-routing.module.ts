a047b2f72365a9e9fcd456535a27a67341a98cf7
jonaznas <git@jonaz.dev>
2021-06-17 22:38:01 +0200

    Add layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from 'src/app/layout/layout.component';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
+
+const topRoutes: Routes = [
+  {
+    path: '',
+    children: []
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: routes
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(topRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from 'src/app/layout/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class LayoutModule { }
