dab411bb61f1393468f606e4e92647059edcc603
jonaznas <git@jonaz.dev>
2021-06-21 05:04:28 +0200

    feat: add player service with websocket connection

---
+++ src/app/layout/player/player-event-dto.ts
@@ -0,0 +1,3 @@
+export type PlayerEventDto = {
+  message: string
+}
---
+++ src/app/layout/player/player.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { PlayerEventDto } from 'src/app/layout/player/player-event-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+  private webSocket?: WebSocket;
+
+  constructor() {
+  }
+
+  async connect(): Promise<void> {
+    let api = localStorage.getItem('api');
+    let token = localStorage.getItem('token');
+
+    if (!api || !token) {
+      return;
+    }
+
+    api = api.replace('https://', 'wss://');
+    api = api.replace('http://', 'ws://');
+
+    this.webSocket =  new WebSocket(`${ api }/player/${ token }`);
+
+    this.webSocket.onmessage = message => this.handleEvent(message);
+  }
+
+  async handleEvent(message: MessageEvent) {
+    const blob = message.data as Blob;
+    const blobText = await blob.text();
+    const event = JSON.parse(blobText) as PlayerEventDto;
+  }
+}
